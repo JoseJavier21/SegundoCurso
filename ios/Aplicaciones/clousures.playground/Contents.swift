aa66ed68e9f6f4c8d06e947b3eaca866ea6d2d39
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2023-11-08 21:26:37 +0100

    ios login

--- ios/Aplicaciones/clousures.playground/Contents.swift
+++ ios/Aplicaciones/clousures.playground/Contents.swift
@@ -1,20 +1,19 @@
 import UIKit
 
-let cosa = {
+var cosa = {
   print("cosa")
 }
 
 var closure = cosa
 
-let suma = {(valor1: Int, valor2: Int) in
+//----------------//
+
+var suma = { (valor1: Int, valor2: Int) in
    return valor1 + valor2
 }
 
 
-//----------------//
-
-
-let suma2 = {(valor1: Int, valor2: Int) in valor1 + valor2 }
+var suma2 = {(valor1: Int, valor2: Int) in valor1 + valor2 }
 
 var sumar = suma2(1,2)
 
@@ -22,26 +21,26 @@ print(sumar)
 
 //----------------//
 
-let cuadrado = {(valor1: Int) -> Int in
+var cuadrado = {(valor1: Int) -> Int in
     return valor1 * valor1
 }
 
 print(cuadrado(5))
 
-let cuadrado2 = {(valor1: Int) in return valor1 * valor1}
+var cuadrado2 = {(valor1: Int) in return valor1 * valor1}
 
 print(cuadrado2(2))
 
 //----------------//
 
 
-let numeros = [23, 4, 36, 25, 23, 46]
+var numeros = [23, 4, 36, 25, 23, 46]
 
-let ordenarArray = { (array: [Int]) in
+var ordenarArray = { (array: [Int]) in
     return array.sorted(by: >)
 }
 
-let ordenarArray2 = { $0.sorted(by: >) }
+var ordenarArray2 = numeros.sorted(by: { $0 < $1})
 
 
 print(ordenarArray(numeros))
@@ -49,9 +48,4 @@ print(ordenarArray(numeros))
 //------------------//
 
 
-let devolverNum = {
-    
-    
-}
-
-
+var _ = {3581}()
