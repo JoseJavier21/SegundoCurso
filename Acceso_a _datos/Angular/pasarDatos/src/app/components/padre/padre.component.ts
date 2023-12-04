516867f27649d6512265edcea99493e6ff8f5736
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2023-12-04 19:26:23 +0100

    ios table layout

---
+++ Acceso_a _datos/Angular/pasarDatos/src/app/components/padre/padre.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-padre',
+  templateUrl: './padre.component.html',
+  styleUrls: ['./padre.component.css']
+})
+export class PadreComponent {
+
+  nombre: String = "Maria"
+
+
+
+}
---
+++ ios/Aplicaciones/TableViewController/TableViewController/Cell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  TableViewController
+//
+//  Created by dam2 on 4/12/23.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    @IBOutlet weak var titleCell: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
