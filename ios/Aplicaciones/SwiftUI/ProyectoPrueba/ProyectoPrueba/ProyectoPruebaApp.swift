9159e9a2d63d122c2aaf196ef881f7417203d3f3
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2024-02-15 16:03:51 +0100

    proyecto pruerba

---
+++ ios/Aplicaciones/SwiftUI/ProyectoPrueba/ProyectoPrueba/ProyectoPruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoPruebaApp.swift
+//  ProyectoPrueba
+//
+//  Created by dam2 on 15/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoPruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
