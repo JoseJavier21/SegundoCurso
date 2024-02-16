1317a9ffa3c3ffac4a29d479a4fe1e69133c7009
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2024-02-16 18:19:22 +0100

    back ejer 2Eval

---
+++ psp/listClientes/src/test/java/com/example/listClientes/ListClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
