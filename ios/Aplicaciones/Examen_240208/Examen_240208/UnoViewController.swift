89c85c5f8a99b3faf20817cce57b25140fd762fa
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2024-02-27 18:38:01 +0100

    tabla ya va

---
+++ ios/Aplicaciones/Examen_240208/Examen_240208/UnoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Examen_240208
+//
+//  Created by Alfonso Marín on 7/2/24.
+//
+
+import UIKit
+
+class UnoViewController: UIViewController {
+
+    @IBOutlet weak var imgage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+
+}
+
